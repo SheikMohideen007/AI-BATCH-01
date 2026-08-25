73c342d4450cc9543834e32feb0c3204d4ec883c
SheikMohideen007 <sheikmohideen20010@gmail.com>
2026-08-25 21:11:06 +0530

    completed till the string functions

---
+++ string_function.py
@@ -0,0 +1,45 @@
+greetings = "Hi, this is pythons course."
+
+length=len(greetings)
+print("The length of the string is ",length)
+
+print(greetings[25])
+print(greetings[0:8]) #Hi, this
+print(greetings[::-1])
+
+
+
+
+
+# Case conversion
+upper_case=greetings.upper()
+print("upper case of the string is : ",upper_case)
+
+lower_case=greetings.lower()
+print("lower case of the string is : ",lower_case)
+
+capitalize_case=greetings.capitalize()
+print("capitalise case of the string is : ",capitalize_case)
+
+title_case=greetings.title()
+print("title case of the string is : ",title_case)
+
+swap_case=greetings.swapcase()
+print("swap case of the string is : ",swap_case)
+
+
+# Searching
+python_index=greetings.find("python")
+print('index of python is ', python_index)
+
+#Todo : index(),endswith()
+
+print(greetings.startswith("HI"))
+
+
+# replace
+print(greetings.replace("pythons","sql"))
+
+
+
+
---
+++ variables.py
@@ -0,0 +1,14 @@
+name="Sheik" # String
+age=10  # integer
+salary=200.50 # float
+isDeveloper=False # boolean type
+
+print(name)
+print(age)
+print(salary)
+print(isDeveloper)
+
+print(type(name))
+print(type(age))
+print(type(salary))
+print(type(isDeveloper))
